d36b35210915c944ad5ee327a5be353f3a2ebb39
Gustav Sundin <gustav@kicksort.se>
2017-03-31 13:41:02 +0200

    Converted lineup section into a recycler view

--- app/src/main/java/se/kicksort/metalarchives/band/AlbumViewHolder.java
+++ app/src/main/java/se/kicksort/metalarchives/band/AlbumViewHolder.java
@@ -8,21 +8,21 @@ import se.kicksort.metalarchives.databinding.AlbumListEntryBinding;
 import se.kicksort.metalarchives.model.Album;
 
 public class AlbumViewHolder extends SortedListAdapter.ViewHolder<Album> {
-    private final AlbumListEntryBinding mBinding;
+    private final AlbumListEntryBinding binding;
     private PublishSubject<Album> clickSubject = PublishSubject.create();
 
     public AlbumViewHolder(AlbumListEntryBinding binding) {
         super(binding.getRoot());
-        mBinding = binding;
-        mBinding.getRoot().setOnClickListener(view -> clickSubject.onNext(mBinding.getAlbum()));
+        this.binding = binding;
+        this.binding.getRoot().setOnClickListener(view -> clickSubject.onNext(this.binding.getAlbum()));
     }
 
     @Override
     public void performBind(Album item) {
-        mBinding.setAlbum(item);
+        binding.setAlbum(item);
     }
 
-    public Observable<Album> getSearchResultClicks() {
+    public Observable<Album> getClicks() {
         return clickSubject;
     }
 }
