831facea3be84c4db1b170722d64e895272131d1
Gustav Sundin <gustav@kicksort.se>
2017-03-31 14:09:36 +0200

    Refactoring

--- app/src/main/java/se/kicksort/metalarchives/band/BandFragment.java
+++ app/src/main/java/se/kicksort/metalarchives/band/BandFragment.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.wrdlbrnft.sortedlistadapter.SortedListAdapter;
 import com.squareup.picasso.Picasso;
 
 import java.util.Comparator;
@@ -48,30 +50,40 @@ public class BandFragment extends Fragment {
             loadBandData();
         }
 
-        final Comparator<Album> CHRONOLOGICAL_COMPARATOR = (a, b) -> a.getYear().compareTo(b.getYear());
-        albumAdapter = new AlbumAdapter(getContext(), CHRONOLOGICAL_COMPARATOR);
-
-        binding.discographyRecyclerView.addItemDecoration(new CustomDividerItemDecoration(getContext()));
-        binding.discographyRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.discographyRecyclerView.setAdapter(albumAdapter);
-
-        albumAdapter.getClicks().subscribe(album -> NavigationManager.getInstance().openAlbum(album.getId()));
-
-        final Comparator<BandMember> ALPHABETICAL_COMPARATOR = (a, b) -> a.getName().compareTo(b.getName());
-        membersAdapter = new MembersAdapter(getContext(), ALPHABETICAL_COMPARATOR);
-
-        binding.membersRecyclerView.addItemDecoration(new CustomDividerItemDecoration(getContext()));
-        binding.membersRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.membersRecyclerView.setAdapter(membersAdapter);
-
-        membersAdapter.getClicks().subscribe(member -> {});
-
-        binding.discographyToggle.setOnValueChangedListener(this::showDiscographySection);
-        binding.membersToggle.setOnValueChangedListener(this::showMembersSection);
+        setupDiscographySection();
+        setupMembersSection();
 
         binding.scrollView.getViewTreeObserver().addOnScrollChangedListener(() -> scrollSubject.onNext(binding.scrollView.getScrollY()));
 
         return binding.getRoot();
+    }
+
+    private void setupDiscographySection() {
+        final Comparator<Album> CHRONOLOGICAL_COMPARATOR = (a, b) -> a.getYear().compareTo(b.getYear());
+        albumAdapter = new AlbumAdapter(getContext(), CHRONOLOGICAL_COMPARATOR);
+        albumAdapter.getClicks().subscribe(album -> NavigationManager.getInstance().openAlbum(album.getId()));
+
+        setupRecyclerView(binding.discographyRecyclerView, albumAdapter);
+
+        binding.discographyToggle.setOnValueChangedListener(this::showDiscographySection);
+    }
+
+    private void setupMembersSection() {
+        final Comparator<BandMember> ALPHABETICAL_COMPARATOR = (a, b) -> a.getName().compareTo(b.getName());
+        membersAdapter = new MembersAdapter(getContext(), ALPHABETICAL_COMPARATOR);
+        membersAdapter.getClicks().subscribe(member -> {
+            // TODO: Clicked band member
+        });
+
+        setupRecyclerView(binding.membersRecyclerView, membersAdapter);
+
+        binding.membersToggle.setOnValueChangedListener(this::showMembersSection);
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView, SortedListAdapter adapter) {
+        recyclerView.addItemDecoration(new CustomDividerItemDecoration(getContext()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
     }
 
     public void setBandId(String id) {
