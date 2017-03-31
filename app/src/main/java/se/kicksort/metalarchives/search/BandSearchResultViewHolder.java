82d0e3651c26eadf2f43b3ce22051b5eb8acb196
Gustav Sundin <gustav@kicksort.se>
2017-03-31 10:44:48 +0200

    Simplified code by using SortedListAdapter

--- app/src/main/java/se/kicksort/metalarchives/search/BandSearchResultViewHolder.java
+++ app/src/main/java/se/kicksort/metalarchives/search/BandSearchResultViewHolder.java
@@ -1,19 +1,28 @@
 package se.kicksort.metalarchives.search;
 
-import android.support.v7.widget.RecyclerView;
+import com.github.wrdlbrnft.sortedlistadapter.SortedListAdapter;
 
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
 import se.kicksort.metalarchives.databinding.BandSearchResultBinding;
 import se.kicksort.metalarchives.model.BandSearchResult;
 
-public class BandSearchResultViewHolder extends RecyclerView.ViewHolder {
+public class BandSearchResultViewHolder extends SortedListAdapter.ViewHolder<BandSearchResult> {
     private final BandSearchResultBinding mBinding;
+    private PublishSubject<BandSearchResult> clickSubject = PublishSubject.create();
 
     public BandSearchResultViewHolder(BandSearchResultBinding binding) {
         super(binding.getRoot());
         mBinding = binding;
+        mBinding.getRoot().setOnClickListener(view -> clickSubject.onNext(mBinding.getModel()));
     }
 
-    public void bind(BandSearchResult item) {
+    @Override
+    public void performBind(BandSearchResult item) {
         mBinding.setModel(item);
+    }
+
+    public Observable<BandSearchResult> getSearchResultClicks() {
+        return clickSubject;
     }
 }
