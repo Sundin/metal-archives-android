6f720abfb33c569582bc5d908eb057812d3c804e
Gustav Sundin <gustav@kicksort.se>
2017-09-22 16:15:54 +0200

    Added Backend class

---
+++ app/src/main/java/se/kicksort/metalarchives/network/Backend.java
@@ -0,0 +1,37 @@
+package se.kicksort.metalarchives.network;
+
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+public class Backend {
+    private final static String baseUrl = "http://192.168.1.64:4567/";
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+
+    private static String getLocalIpAddress() throws SocketException, UnknownHostException{
+        InetAddress result = null;
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            Enumeration<InetAddress> addresses = interfaces.nextElement().getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (!address.isLoopbackAddress()) {
+                    if (address.isSiteLocalAddress()) {
+                        return address.getHostAddress();
+                    } else if (result == null) {
+                        result = address;
+                    }
+                }
+            }
+        }
+
+        return (result != null ? result : InetAddress.getLocalHost()).getHostAddress();
+    }
+}
